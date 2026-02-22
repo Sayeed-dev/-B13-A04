94b9bb96a79d08c6a368226d557f628948077f69
Sheikh Samaun Sayeed <samaunsayeed50@gmail.com>
2026-02-23 03:30:10 +0600

    Added Toggles Functionalities

--- script/app.js
+++ script/app.js
@@ -1,26 +1,37 @@
 // Tracking Area Selectors
 let total = document.getElementById('total-counter');
-let interview = document.getElementById('interview-counter')
-let rejected = document.getElementById('rejected-counter')
+let interview = document.getElementById('interview-counter');
+let rejected = document.getElementById('rejected-counter');
 
 // Toggle Button Area Selectors
-const toggleContainer = document.querySelector('.toggle-btn-area')
+const toggleContainer = document.querySelector('.toggle-buttons');
 
 // Cards Selector
-const cardsContainer = document.querySelector('.cards')
-
+const cardsContainer = document.querySelector('.cards');
 
 //--------------------------
 //--------------------------
 
 // Empty Arrays
-let interviewCollection = []
-let rejectedCollection = []
+let interviewCollection = [];
+let rejectedCollection = [];
 
 // Fuction
-function Count () {
-  total.innerText = cardsContainer.children.length
-  interview.innerText = interviewCollection.length
-  rejected.innerHTML = rejectedCollection.length
+function Count() {
+  total.innerText = cardsContainer.children.length;
+  interview.innerText = interviewCollection.length;
+  rejected.innerHTML = rejectedCollection.length;
 }
-Count()
+Count();
+
+// Functionality for Toggle Buttons
+for (const button of toggleContainer.children) {
+  button.addEventListener('click', function () {
+    for (const btn of toggleContainer.children) {
+      btn.classList.remove('bg-blue-950', 'text-blue-50');
+      btn.classList.add('text-blue-950', 'bg-blue-50');
+    }
+    button.classList.remove('text-blue-950', 'bg-blue-50');
+    button.classList.add('bg-blue-950', 'text-blue-50');
+  });
+}
