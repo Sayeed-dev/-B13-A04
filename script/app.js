138d07df7213939ce5bd72c418b5376705ef8407
Sheikh Samaun Sayeed <samaunsayeed50@gmail.com>
2026-02-23 02:49:05 +0600

    Added Some JS Varisbles

--- script/app.js
+++ script/app.js
@@ -1,4 +1,26 @@
+// Tracking Area Selectors
 let total = document.getElementById('total-counter');
 let interview = document.getElementById('interview-counter')
 let rejected = document.getElementById('rejected-counter')
 
+// Toggle Button Area Selectors
+const toggleContainer = document.querySelector('.toggle-btn-area')
+
+// Cards Selector
+const cardsContainer = document.querySelector('.cards')
+
+
+//--------------------------
+//--------------------------
+
+// Empty Arrays
+let interviewCollection = []
+let rejectedCollection = []
+
+// Fuction
+function Count () {
+  total.innerText = cardsContainer.children.length
+  interview.innerText = interviewCollection.length
+  rejected.innerHTML = rejectedCollection.length
+}
+Count()
